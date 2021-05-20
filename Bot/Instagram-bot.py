44cc330990aa1156c65028a1b5d7f492dc108800
SHREYAN SAHA <76966111+sahashreyan@users.noreply.github.com>
2021-05-20 17:19:54 +0530

    Comments added

--- Bot/Instagram-bot.py
+++ Bot/Instagram-bot.py
@@ -1,11 +1,16 @@
 from instapy import InstaPy
 
-session = InstaPy(username = "r.i.c.h.i.k", password = "$Richik1234")
+session = InstaPy(username = "your username", password = "your password")
+#Replace your username with your actual username and the same with your password
 session.login()
 
 session.set_relationship_bounds(enabled = True, max_followers = 200)
+#You can set max followers to limit the search to those accounts within the limit
 session.set_do_follow(True, percentage=100)
+
 session.like_by_tags(["fcbarcelona", "poetrycommunity", "fcbfemeni", "fcb"], amount = 3)
+#add as many tags as you want and remember the actual amount will be 9 more than what is given.
 session.set_dont_like(["nsfw"])
+#here is the list of tags you want to exclude from your search
 
 session.end()
