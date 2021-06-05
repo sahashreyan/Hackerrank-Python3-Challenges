85ef06aa8037c61b5cc193b8ac6b469f64af1a5e
SHREYAN SAHA <76966111+sahashreyan@users.noreply.github.com>
2021-06-05 22:40:44 +0530

    Initial Commit

---
+++ Animations/spiral.py
@@ -0,0 +1,12 @@
+import turtle
+colors = ["red","blue","green","orange","pink","white","yellow"]
+screen = turtle.Screen()
+t = turtle.Turtle()
+t.speed(0)
+screen.bgcolor("black")
+for x in range(300):
+    t.pencolor(colors[x%7])
+    t.width(3)
+    t.forward(x)
+    t.left(20)
+    
