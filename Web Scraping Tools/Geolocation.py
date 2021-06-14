474d3c8c2e90d630cf8a496ea218940ce04d922b
SHREYAN SAHA <76966111+sahashreyan@users.noreply.github.com>
2021-06-14 21:00:56 +0530

    Initial Commit

---
+++ Web Scraping Tools/Geolocation.py
@@ -0,0 +1,26 @@
+from geopy.geocoders import Nominatim
+import time
+from pprint import pprint
+
+# instantiate a new Nominatim client
+app = Nominatim(user_agent="tutorial")
+
+#get location raw data
+location = app.geocode("Nairobi, Kenya").raw
+
+pprint(location)
+
+def get_location(address):
+    time.sleep(1)
+    try:
+        return app.geocode(address).raw
+    except:
+        return get_location(address)
+#Enter the address to search
+address = "Makai Road, Masaki, Dar es Salaam, Tanzania"
+location = get_location(address)
+latitude = location["lat"]
+longitude = location["lon"]
+print(f"{latitude}, {longitude}")
+
+pprint(location)
