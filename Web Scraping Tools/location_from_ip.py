999ed764a274e3fae2092e957f1e2d4c35e8bd11
SHREYAN SAHA <76966111+sahashreyan@users.noreply.github.com>
2021-07-05 10:33:11 +0530

    Initial Commit

---
+++ Web Scraping Tools/location_from_ip.py
@@ -0,0 +1,11 @@
+import socket
+from ip2geotools.databases.noncommercial import DbIpCity
+
+url = input("Enter an URL: ")
+ip = socket.gethostbyname(url)
+response = DbIpCity.get(ip, api_key='free')
+
+print("IP: ", ip)
+print("City: ", response.city)
+print("Region: ", response.region)
+print("Country: ", response.country)
